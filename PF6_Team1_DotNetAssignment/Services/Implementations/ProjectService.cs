e4a02b690d0a11b5fe4af14be0e1429d7bd6345b
Giannos-G <giannos.gavrielides@gmail.com>
2021-05-26 02:23:47 +0300

    Interface and Implementations of Projects - First Commit

---
+++ PF6_Team1_DotNetAssignment/Services/Implementations/ProjectService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PF6_Team1_DotNetAssignment.Services.Implementations
+{
+    public class ProjectService
+    {
+    }
+}
