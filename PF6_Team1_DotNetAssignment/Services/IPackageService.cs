aec8a9b5b54fed420dcd4fd5aa3f024315b345d0
billgero <48282069+billgero@users.noreply.github.com>
2021-06-03 15:25:05 +0300

    Update IPackageService.cs

--- PF6_Team1_DotNetAssignment/Services/IPackageService.cs
+++ PF6_Team1_DotNetAssignment/Services/IPackageService.cs
@@ -1,8 +1,5 @@
-<<<<<<< HEAD:PF6_Team1_DotNetAssignment/Services/IPackageService.cs
-﻿using PF6_Team1_DotNetAssignment.Options;
-=======
 using PF6_Team1_DotNetAssignment.Options;
->>>>>>> 8bbe619dd7c94e131f44c7575544a84ea36a94ad:PF6_Team1_DotNetAssignment/Services/IPackageService
+using PF6_Team1_DotNetAssignment.Options;
 using System.Threading.Tasks;
 
 namespace PF6_Team1_DotNetAssignment.Services
